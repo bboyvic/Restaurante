576ada85e7314904b8f09959271f5d9cb0a76b3e
bboyvic <vic.rivas.1999@gmail.com>
2019-12-13 02:44:54 -0600

    CREACION DE LA VISTA ERROR 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ALGO SALIO MAL</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<center>
+	<a href="{{route('arboleda.index')}}"><button type="button" class="btn btn-danger btn-lg btn-block"><strong style="color: black;">VOLVER AL INICIO</strong></button></a>
+	<br>
+	<img src="{{asset('imgs/error404.jpg')}}" style="width:70%">
+</center>
+</body>
+</html>
